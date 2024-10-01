7c7cc86a47ebde79902c0d067ec352d64d22969c
Ehioje Henry Erabor <ehiojee@yahoo.com>
2024-10-02 00:57:31 +0100

    create function to acquire lock

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
+
+func acquireLock(client *redis.Client, lockKey string, timeout time.Duration) bool {
+	ctx := context.Background()
+
+	// Try to acquire the lock with SETNX command (SET if Not eXist)
+	lockAcquired, err := client.SetNX(ctx, lockKey, "1", timeout).Result()
+	if err != nil {
+		fmt.Println("Error acquiring lock")
+		return false
+	}
+
+	return lockAcquired
+
+}
 
 func main() {
 	fmt.Println("Hello Redis Go Lock!")
